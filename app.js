0e515a97232766c2513ea734f889d15aeec46aae
skara-glitch <uzairahamed6@gmail.com>
2021-05-03 18:47:57 +0530

    sell

---
+++ app.js
@@ -0,0 +1,28 @@
+name: mask;
+count: shopping-cart;
+images: [{
+    URL:('https://cdn.quicksell.co/-LfPe0CWGEWcVYQTYbDw/products/-MVpx91iKhzu-4c2hPFN.jpg'),
+  ready : true,
+  error: false
+}]
+name: mask;
+count: shopping-cart;
+images: [{
+    URL:('https://cdn.quicksell.co/-LfPe0CWGEWcVYQTYbDw/products/-MVpx91hJWM9aEdBMICb.jpg'),
+  ready : true,
+  error: false
+}]
+name: mask;
+count: shopping-cart;
+images: [{
+    URL:('https://cdn.quicksell.co/-LfPe0CWGEWcVYQTYbDw/products/-MVpx91j5-eCEW5j1nOY.jpg'),
+  ready : true,
+  error: false
+}]
+name: mask;
+count: shopping-cart;
+images: [{
+    URL:('https://cdn.quicksell.co/-LfPe0CWGEWcVYQTYbDw/products/-MVpx91hJWM9aEdBMICd.jpg'),
+  ready : true,
+  error: false
+}]
